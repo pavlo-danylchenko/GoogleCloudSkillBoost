24f55ee1e1eca7841cea8739b7b46f32ad9b01fd
Pavlo Danylchenko <danilchenko@ukr.net>
2026-02-03 23:58:35 -0500

    Complete GSP 004

--- creating_a_persistent_disk/gsp004.sh
+++ creating_a_persistent_disk/gsp004.sh
@@ -32,8 +32,8 @@ echo "----------------------------------------------------------------------"
 echo "          Finding the persistent disk in the virtual machine"
 echo "----------------------------------------------------------------------"
 gcloud compute ssh gcelab --zone $ZONE --quiet \
-    --command="ls -l /dev/disk/by-id \
-    sudo mkdir /mnt/mydisk \
+    --command="ls -l /dev/disk/by-id &&\
+    sudo mkdir /mnt/mydisk &&\
     sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 \
     && sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk"
 
