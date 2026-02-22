cd8e165e5d618ce2af0177b47c82af8f302d1dc3
Pavlo Danylchenko <danilchenko@ukr.net>
2026-02-22 15:51:17 -0500

    Complete Enhance Scalability Using Managed Instance Groups lab

--- enhance_scalability_using_managed_instance_groups/gsp.sh
+++ enhance_scalability_using_managed_instance_groups/gsp.sh
@@ -1,14 +1,4 @@
-export ZONE=$(gcloud compute project-info describe \
-    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+export REGION=us-west1
 
-gcloud compute instance-groups managed create dev-instance-group \
-    --zone=$ZONE \
-    --template=dev-instance-template \
-    --size=1
-gcloud compute instance-groups managed set-autoscaling dev-instance-group \
-    --zone=$ZONE \
-    --mode=on \
-    --min-num-replicas=1 \
-    --max-num-replicas=3 \
-    --target-cpu-utilization=0.6
+gcloud compute instance-groups managed create dev-instance-group --region=REGION --template=dev-instance-template --size=1
+gcloud compute instance-groups managed set-autoscaling dev-instance-group --region=$REGION --mode=on --min-num-replicas=1 --max-num-replicas=3 --target-cpu-utilization=0.6
