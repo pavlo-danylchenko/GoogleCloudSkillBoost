4bc2cf9d161a6d9315fd484b199613998d1667bd
Pavlo Danylchenko <danilchenko@ukr.net>
2026-02-27 17:33:31 -0500

    Complete GSP 1050

--- cloud_spanner_defining_schemas_and_understanding_query_plans/gsp1050.sh
+++ cloud_spanner_defining_schemas_and_understanding_query_plans/gsp1050.sh
@@ -5,22 +5,22 @@ echo "======================================================================"
 echo "                 Task 1. Load data into tables"
 echo "======================================================================"
 SQL_QUERIES="
-        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (1, "Banking", "Bnkg", "All Banking Business");
-        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (2, "Asset Growth", "AsstGrwth", "All Asset Focused Products");
-        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (3, "Insurance", "Ins", "All Insurance Focused Products");
-        insert into Category (CategoryId,PortfolioId,CategoryName) values (1,1,"Cash");
-        insert into Category (CategoryId,PortfolioId,CategoryName) values (2,2,"Investments - Short Return");
-        insert into Category (CategoryId,PortfolioId,CategoryName) values (3,2,"Annuities");
-        insert into Category (CategoryId,PortfolioId,CategoryName) values (4,3,"Life Insurance");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (1,1,1,"Checking Account","ChkAcct","Banking LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (2,2,2,"Mutual Fund Consumer Goods","MFundCG","Investment LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (3,3,2,"Annuity Early Retirement","AnnuFixed","Investment LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (4,4,3,"Term Life Insurance","TermLife","Insurance LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (5,1,1,"Savings Account","SavAcct","Banking LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (6,1,1,"Personal Loan","PersLn","Banking LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (7,1,1,"Auto Loan","AutLn","Banking LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (8,4,3,"Permanent Life Insurance","PermLife","Insurance LOB");
-        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (9,2,2,"US Savings Bonds","USSavBond","Investment LOB");
+        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (1, 'Banking', 'Bnkg', 'All Banking Business');
+        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (2, 'Asset Growth', 'AsstGrwth', 'All Asset Focused Products');
+        insert into Portfolio (PortfolioId, Name, ShortName, PortfolioInfo) values (3, 'Insurance', 'Ins', 'All Insurance Focused Products');
+        insert into Category (CategoryId,PortfolioId,CategoryName) values (1,1,'Cash');
+        insert into Category (CategoryId,PortfolioId,CategoryName) values (2,2,'Investments - Short Return');
+        insert into Category (CategoryId,PortfolioId,CategoryName) values (3,2,'Annuities');
+        insert into Category (CategoryId,PortfolioId,CategoryName) values (4,3,'Life Insurance');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (1,1,1,'Checking Account','ChkAcct','Banking LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (2,2,2,'Mutual Fund Consumer Goods','MFundCG','Investment LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (3,3,2,'Annuity Early Retirement','AnnuFixed','Investment LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (4,4,3,'Term Life Insurance','TermLife','Insurance LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (5,1,1,'Savings Account','SavAcct','Banking LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (6,1,1,'Personal Loan','PersLn','Banking LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (7,1,1,'Auto Loan','AutLn','Banking LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (8,4,3,'Permanent Life Insurance','PermLife','Insurance LOB');
+        insert into Product (ProductId,CategoryId,PortfolioId,ProductName,ProductAssetCode,ProductClass) values (9,2,2,'US Savings Bonds','USSavBond','Investment LOB');
 "
 
 IFS=';' read -ra ADDR <<< "$SQL_QUERIES"
