e649421fdbc9e3e99adada697e5481d854e17d15
Pavlo Danylchenko <danilchenko@ukr.net>
2026-02-28 04:25:47 -0500

    Complete GSP 154

--- video_intelligence_qwik_start/gsp154.sh
+++ video_intelligence_qwik_start/gsp154.sh
@@ -6,8 +6,8 @@ echo "            Task 0. Detecting project IDs, regions and zones"
 echo "                     Setting up the environment"
 echo "======================================================================"
 export PROJECT_ID=$(gcloud config get-value project)
-sudo apt-get update
-sudo apt-get install jq -y
+# sudo apt-get update
+# sudo apt-get install jq -y
 
 echo "======================================================================"
 echo "                   Task 1. Set up authorization"
