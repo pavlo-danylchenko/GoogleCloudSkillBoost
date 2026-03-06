6efee8e02dd9948d3187b43e7c7f8dd3e4286b0a
Pavlo Danylchenko <danilchenko@ukr.net>
2026-03-05 19:33:03 -0500

    Complete GSP 070

---
+++ app_engine_qwik_start_go/gsp070.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "======================================================================"
+echo "            Task 0. Detecting project IDs, regions and zones"
+echo "                     Setting up the environment"
+echo "======================================================================"
+export PROJECT_ID=$(gcloud config get-value project)
+
+export ZONE=$(gcloud compute project-info describe \
+    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+echo $ZONE
+
+export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
+
+
+echo "======================================================================"
+echo "              Task 1. Enable Google App Engine Admin API"
+echo "======================================================================"
+gcloud services enable appengine.googleapis.com
+
+
+echo "======================================================================"
+echo "              Task 2. Download the Hello World app"
+echo "======================================================================"
+git clone https://github.com/GoogleCloudPlatform/golang-samples.git
+cd golang-samples/appengine/go11x/helloworld
+
+sed -i 's/^runtime: php.*/runtime: php83/' app.yaml
+grep runtime app.yaml
+
+
+echo "======================================================================"
+echo "                 Task 3. Deploy your app"
+echo "======================================================================"
+sudo apt-get install google-cloud-sdk-app-engine-go
+
+gcloud app create --project=$PROJECT_ID --region=$REGION
+gcloud app deploy --project $PROJECT_ID --quiet
+
+
+echo "======================================================================"
+echo "                 Task 4. View your application"
+echo "======================================================================"
+gcloud app browse
+
+
+echo "======================================================================"
+echo "                          JOB is DONE !"
+echo "======================================================================"
