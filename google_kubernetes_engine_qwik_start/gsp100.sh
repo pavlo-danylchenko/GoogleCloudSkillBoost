231d14c6800493d4696e965f2543c7ff191b4750
Pavlo Danylchenko <danilchenko@ukr.net>
2026-03-29 14:43:23 -0400

    Complete GSP 100

--- google_kubernetes_engine_qwik_start/gsp100.sh
+++ google_kubernetes_engine_qwik_start/gsp100.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
+set -euo pipefail
 
-# Task 1. Set a default compute zone
+echo "======================================================================"
+echo "            Task 0. Detecting project IDs, regions and zones"
+echo "                     Setting up the environment"
+echo "======================================================================"
+export ZONE=$(gcloud compute project-info describe \
+    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+
+
+echo "======================================================================"
+echo "                Task 1. Set a default compute zone"
+echo "======================================================================"
 gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
 
-# Task 2. Create a GKE cluster
+
+echo "======================================================================"
+echo "                  Task 2. Create a GKE cluster"
+echo "======================================================================"
 gcloud container clusters create --machine-type=e2-medium --zone=$ZONE lab-cluster
 
-# Task 3. Get authentication credentials for the cluster
+
+echo "======================================================================"
+echo "         Task 3. Get authentication credentials for the cluster"
+echo "======================================================================"
 gcloud container clusters get-credentials lab-cluster
 
-# Task 4. Deploy an application to the cluster
+
+echo "======================================================================"
+echo "            Task 4. Deploy an application to the cluster"
+echo "======================================================================"
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 
-# Task 5. Delete the cluster
-sleep 70
+export EXTERNAL_IP=$(kubectl get svc tracing-demo -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "External IP is: $EXTERNAL_IP"
+curl -s http://$EXTERNAL_IP:8080
+
+read -p "Check the progress to verify the objective and Press [Enter] key to continue..."
+
+
+echo "======================================================================"
+echo "                     Task 5. Delete the cluster"
+echo "======================================================================"
 gcloud container clusters delete lab-cluster
 
-echo "Job is Done!"
+
+echo "======================================================================"
+echo "                           JOB is DONE !"
+echo "======================================================================"
