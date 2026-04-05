3cce643828c95f77a29bea29b4f6a17cfe393e8e
Pavlo Danylchenko <danilchenko@ukr.net>
2026-04-05 12:54:27 -0400

    Complete GSP 694

--- gcloud_for_network_configuration/gsp694.sh
+++ gcloud_for_network_configuration/gsp694.sh
@@ -2,6 +2,20 @@
 set -euo pipefail
 
 echo "======================================================================"
+echo "            Task 0. Detecting project IDs, regions and zones"
+echo "                     Setting up the environment"
+echo "======================================================================"
+export ZONE=$(gcloud compute project-info describe \
+    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+echo $ZONE
+
+export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
+
+
+echo "======================================================================"
 echo "                     Task 1. Viewing networks"
 echo "======================================================================"
 gcloud compute networks list
@@ -18,6 +32,9 @@ gcloud compute networks subnets list
 echo "======================================================================"
 echo "                     Task 3. Describe a subnet"
 echo "======================================================================"
+gcloud compute networks subnets describe labnet-sub \
+    --region=$REGION
+
 
 echo "======================================================================"
 echo "                     Task 4. Creating firewall rules"
