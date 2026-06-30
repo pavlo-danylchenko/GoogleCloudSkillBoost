fc7a0a31c5faa1c5097ee8a3019eb905034e3afb
Pavlo Danylchenko <danilchenko@ukr.net>
2026-06-30 11:46:08 -0400

    Complete GSP 222

---
+++ it_speaks_create_synthetic_speech_using_text_to_speech/gsp222.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+set -euo pipefail
+
+
+echo "======================================================================"
+echo "            Task 0. Detecting project IDs, regions and zones"
+echo "                     Setting up the environment"
+echo "======================================================================"
+
+export ZONE=$(gcloud compute project-info describe \
+    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+
+echo $ZONE
+echo $REGION
+
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
+
+
+echo "======================================================================"
+echo "                  Task 1. Enable the Text-to-Speech API"
+echo "======================================================================"
+gcloud services enable texttospeech.googleapis.com
+sleep 5
+
+
+echo "======================================================================"
+echo "                  Task 2. Create a virtual environment"
+echo "======================================================================"
+sudo apt-get install -y virtualenv
+python3 -m venv venv
+source venv/bin/activate
+
+
+echo "======================================================================"
+echo "                  Task 3. Create a service account"
+echo "======================================================================"
+gcloud iam service-accounts create tts-qwiklab
+gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+export ACCESS_TOKEN=$(gcloud auth print-access-token)
+
+echo "======================================================================"
+echo "                Task 4. Get a list of available voices"
+echo "======================================================================"
+
+
+echo "======================================================================"
+echo "                Task 5. Create synthetic speech from text"
+echo "======================================================================"
+cat > synthesize-text.json << EOF
+{
+    'input':{
+        'text':'Cloud Text-to-Speech API allows developers to include
+           natural-sounding, synthetic human speech as playable audio in
+           their applications. The Text-to-Speech API converts text or
+           Speech Synthesis Markup Language (SSML) input into audio data
+           like MP3 or LINEAR16 (the encoding used in WAV files).'
+    },
+    'voice':{
+        'languageCode':'en-gb',
+        'name':'en-GB-Standard-A',
+        'ssmlGender':'FEMALE'
+    },
+    'audioConfig':{
+        'audioEncoding':'MP3'
+    }
+}
+EOF
+
+curl -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @synthesize-text.json "https://texttospeech.googleapis.com/v1/text:synthesize" \
+  > synthesize-text.txt
+
+echo "----------------------------------------------------------------------"
+echo "                     Translate the response"
+echo "----------------------------------------------------------------------"
+cat > tts_decode.py << EOF
+import argparse
+from base64 import decodebytes
+import json
+
+"""
+Usage:
+        python tts_decode.py --input "synthesize-text.txt" \
+        --output "synthesize-text-audio.mp3"
+
+"""
+
+def decode_tts_output(input_file, output_file):
+    """ Decode output from Cloud Text-to-Speech.
+
+    input_file: the response from Cloud Text-to-Speech
+    output_file: the name of the audio file to create
+
+    """
+
+    with open(input_file) as input:
+        response = json.load(input)
+        audio_data = response['audioContent']
+
+        with open(output_file, "wb") as new_file:
+            new_file.write(decodebytes(audio_data.encode('utf-8')))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Decode output from Cloud Text-to-Speech",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--input',
+                       help='The response from the Text-to-Speech API.',
+                       required=True)
+    parser.add_argument('--output',
+                       help='The name of the audio file to create',
+                       required=True)
+
+    args = parser.parse_args()
+    decode_tts_output(args.input, args.output)
+EOF
+
+echo "----------------------------------------------------------------------"
+echo "                    Create and run the audio file"
+echo "----------------------------------------------------------------------"
+python tts_decode.py --input "synthesize-text.txt" --output "synthesize-text-audio.mp3"
+
+
+echo "======================================================================"
+echo "                Task 6. Create synthetic speech from SSML"
+echo "======================================================================"
+cat > synthesize-ssml.json << EOF
+{
+    'input':{
+        'ssml':'<speak><s>
+           <emphasis level="moderate">Cloud Text-to-Speech API</emphasis>
+           allows developers to include natural-sounding
+           <break strength="x-weak"/>
+           synthetic human speech as playable audio in their
+           applications.</s>
+           <s>The Text-to-Speech API converts text or
+           <prosody rate="slow">Speech Synthesis Markup Language</prosody>
+           <say-as interpret-as=\"characters\">SSML</say-as>
+           input into audio data
+           like <say-as interpret-as=\"characters\">MP3</say-as> or
+           <sub alias="linear sixteen">LINEAR16</sub>
+           <break strength="weak"/>
+           (the encoding used in
+           <sub alias="wave">WAV</sub> files).</s></speak>'
+    },
+    'voice':{
+        'languageCode':'en-gb',
+        'name':'en-GB-Standard-A',
+        'ssmlGender':'FEMALE'
+    },
+    'audioConfig':{
+        'audioEncoding':'MP3'
+    }
+}
+EOF
+
+curl -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @synthesize-ssml.json "https://texttospeech.googleapis.com/v1/text:synthesize" \
+  > synthesize-ssml.txt
+
+
+echo "----------------------------------------------------------------------"
+echo "                    Create and run the audio file"
+echo "----------------------------------------------------------------------"
+python tts_decode.py --input "synthesize-ssml.txt" --output "synthesize-ssml-audio.mp3"
+
+echo "======================================================================"
+echo "           Task 7. Configure audio output and device profiles"
+echo "======================================================================"
+cat > synthesize-with-settings.json << EOF
+{
+    'input':{
+        'text':'The Text-to-Speech API is ideal for any application
+          that plays audio of human speech to users. It allows you
+          to convert arbitrary strings, words, and sentences into
+          the sound of a person speaking the same things.'
+    },
+    'voice':{
+        'languageCode':'en-us',
+        'name':'en-GB-Standard-A',
+        'ssmlGender':'FEMALE'
+    },
+    'audioConfig':{
+      'speakingRate': 1.15,
+      'pitch': -2,
+      'audioEncoding':'OGG_OPUS',
+      'effectsProfileId': ['headphone-class-device']
+    }
+}
+EOF
+
+curl -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @synthesize-with-settings.json "https://texttospeech.googleapis.com/v1beta1/text:synthesize" \
+  > synthesize-with-settings.txt
+
+
+echo "----------------------------------------------------------------------"
+echo "                    Create and run the audio file"
+echo "----------------------------------------------------------------------"
+python3 tts_decode.py --input "synthesize-with-settings.txt" --output "synthesize-with-settings-audio.mp3"
+
+echo "======================================================================"
+echo "                          JOB is DONE !"
+echo "======================================================================"
