29774315d5aaaf3b18a11b77236c41a51a74b5cc
Pavlo Danylchenko <danilchenko@ukr.net>
2026-08-28 16:21:06 -0400

    Complete GSP 736

--- debug_apps_on_google_kubernetes_engine/gsp736.sh
+++ debug_apps_on_google_kubernetes_engine/gsp736.sh
@@ -15,8 +15,8 @@ gcloud config set compute/region $REGION
 echo "======================================================================"
 echo "                 Task 1. Perform infrastructure setup"
 echo "======================================================================"
-gcloud services enable cloudaicompanion.googleapis.com
-gcloud container clusters list
+# gcloud services enable cloudaicompanion.googleapis.com
+# gcloud container clusters list
 
 
 echo "======================================================================"
@@ -28,16 +28,60 @@ cd microservices-demo
 gcloud container clusters get-credentials central --zone $ZONE
 kubectl apply -f release/kubernetes-manifests.yaml
 
-kubectl get nodes
+sleep 10
 
-export EXTERNAL_IP=$(kubectl get service frontend-external | awk 'BEGIN { cnt=0; } { cnt+=1; if (cnt > 1) print $4; }')
-curl -o /dev/null -s -w "%{http_code}\n"  http://$EXTERNAL_IP
+# kubectl get nodes
+
+# export EXTERNAL_IP=$(kubectl get service frontend-external | awk 'BEGIN { cnt=0; } { cnt+=1; if (cnt > 1) print $4; }')
+# curl -o /dev/null -s -w "%{http_code}\n"  http://$EXTERNAL_IP
 
 
 echo "======================================================================"
 echo "                 Task 4. Create a logs-based metric"
 echo "======================================================================"
-read -p "Complete task manually and press ENTER to continue..."
+gcloud logging metrics create Error_Rate_SLI \
+  --description="Error rate for recommendationservice" \
+  --log-filter="resource.type=\"k8s_container\" severity=ERROR labels.\"k8s-pod/app\": \"recommendationservice\""
+
+sleep 10
+
+cat > policy-config.json << EOF
+{
+  "displayName": "Error Rate SLI",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "Kubernetes Container - logging/user/Error_Rate_SLI",
+      "conditionThreshold": {
+        "filter": "resource.type = \"k8s_container\" AND metric.type = \"logging.googleapis.com/user/Error_Rate_SLI\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 0.5
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "604800s"
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF
+
+gcloud monitoring policies create --policy-from-file="policy-config.json"
+
 
 echo "======================================================================"
 echo "                       JOB is DONE !!!"
