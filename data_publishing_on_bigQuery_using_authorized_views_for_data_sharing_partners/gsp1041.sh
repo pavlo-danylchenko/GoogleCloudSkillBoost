62bc0891b0112bf6601469506146b45856556826
Pavlo Danylchenko <danilchenko@ukr.net>
2026-08-29 19:35:40 -0400

    Complete GSP 1041

---
+++ data_publishing_on_bigQuery_using_authorized_views_for_data_sharing_partners/gsp1041.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "======================================================================"
+echo "            Task 0. Detecting project IDs, regions and zones"
+echo "                     Setting up the environment"
+echo "======================================================================"
+
+export ZONE=$(gcloud compute project-info describe \
+    --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(echo $ZONE | cut -d '-' -f 1-2)
+
+echo $ZONE
+echo $REGION
+
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
+
+read -p "ENTER the PARTNER Customer: " PARTNER_CUSTOMER
+read -p "ENTER the PARTNER_PROJECT: " PARTNER_PROJECT
+read -p "ENTER the Customer A: " CUSTOMER_A
+read -p "ENTER the PROJECT A: " PROJECT_A
+read -p "ENTER the Customer B: " CUSTOMER_B
+read -p "ENTER the PROJECT B: " PROJECT_B
+
+echo "======================================================================"
+echo "                    Task 1. Create authorized views"
+echo "======================================================================"
+bq mk \
+    --use_legacy_sql=false \
+    --view='
+        SELECT *
+        FROM `bigquery-public-data.geo_us_boundaries.zip_codes`
+        WHERE state_code = "TX"
+        LIMIT 4000
+    ' \
+    "$DEVSHELL_PROJECT_ID:demo_dataset.authorized_view_a"
+
+bq mk \
+    --use_legacy_sql=false \
+    --view='
+        SELECT *
+        FROM `bigquery-public-data.geo_us_boundaries.zip_codes`
+        WHERE state_code = "CA"
+        LIMIT 4000
+    ' \
+    "$DEVSHELL_PROJECT_ID:demo_dataset.authorized_view_b"
+
+
+echo "======================================================================"
+echo "         Task 2. Assign IAM permissions to both the views"
+echo "======================================================================"
+PROJECT_ID=$(gcloud config get-value project)
+
+bq show --format=prettyjson \
+    "$DEVSHELL_PROJECT_ID:demo_dataset" > dataset.json
+
+jq --arg project "$DEVSHELL_PROJECT_ID" '
+  .access += [
+    {
+      "view": {
+        "projectId": $project,
+        "datasetId": "demo_dataset",
+        "tableId": "authorized_view_a"
+      }
+    },
+    {
+      "view": {
+        "projectId": $project,
+        "datasetId": "demo_dataset",
+        "tableId": "authorized_view_b"
+      }
+    }
+  ]
+' dataset.json > dataset-updated.json
+
+bq update \
+    --source dataset-updated.json \
+    "$DEVSHELL_PROJECT_ID:demo_dataset"
+
+echo "======================================================================"
+echo "     Task 3. Grant permissions to the users to access the views"
+echo "======================================================================"
+bq add-iam-policy-binding \
+    "$DEVSHELL_PROJECT_ID:demo_dataset.authorized_view_a" \
+    --member="user:$CUSTOMER_A" \
+    --role="roles/bigquery.dataViewer"
+
+bq add-iam-policy-binding \
+    "$DEVSHELL_PROJECT_ID:demo_dataset.authorized_view_b" \
+    --member="user:$CUSTOMER_B" \
+    --role="roles/bigquery.dataViewer"
+
+
+echo "======================================================================"
+echo "      Task 4. Verify shared authorized views in customer projects"
+echo "======================================================================"
+echo "----------------------------------------------------------------------"
+echo "             Verify authorized view sharing for Customer A"
+echo "----------------------------------------------------------------------"
+
+gcloud auth login $CUSTOMER_A \
+    --no-launch-browser \
+    --quiet
+
+gcloud config set account $CUSTOMER_A
+
+bq query \
+    --project=$PROJECT_A \
+    --use_legacy_sql=false \
+    "SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_a\`"
+
+bq mk \
+    --use_legacy_sql=false \
+    --view="SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_a\`" \
+  "${PROJECT_A}:customer_a_dataset.customer_a_table"
+
+bq query \
+    --project=$PROJECT_A \
+    --use_legacy_sql=false \
+    "
+    SELECT
+        geos.zip_code,
+        geos.city,
+        cust.last_name,
+        cust.first_name
+    FROM \`${PROJECT_A}.customer_a_dataset.customer_info\` AS cust
+    JOIN \`${PARTNER_PROJECT}.demo_dataset.authorized_view_a\` AS geos
+    ON geos.zip_code = cust.postal_code
+    "
+
+# bq query \
+#     --use_legacy_sql=false \
+#     "SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_b\`"
+
+echo "----------------------------------------------------------------------"
+echo "             Verify authorized view sharing for Customer B"
+echo "----------------------------------------------------------------------"
+
+gcloud auth login $CUSTOMER_B \
+    --no-launch-browser \
+    --quiet
+
+gcloud config set account $CUSTOMER_B
+
+bq query \
+    --project=$PROJECT_B \
+    --use_legacy_sql=false \
+    "SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_b\`"
+
+bq mk \
+    --use_legacy_sql=false \
+    --view="SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_a\`" \
+  "${PROJECT_B}:customer_b_dataset.customer_b_table"
+
+bq query \
+    --project=$PROJECT_B \
+    --use_legacy_sql=false \
+    "
+    SELECT
+        geos.zip_code,
+        geos.city,
+        cust.last_name,
+        cust.first_name
+    FROM \`${PROJECT_B}.customer_b_dataset.customer_info\` AS cust
+    JOIN \`${PARTNER_PROJECT}.demo_dataset.authorized_view_b\` AS geos
+    ON geos.zip_code = cust.postal_code
+    "
+
+# bq query \
+#     --use_legacy_sql=false \
+#     "SELECT * FROM \`${PARTNER_PROJECT}.demo_dataset.authorized_view_b\`"
+
+
+echo "======================================================================"
+echo "                          JOB is DONE !!!"
+echo "======================================================================"
