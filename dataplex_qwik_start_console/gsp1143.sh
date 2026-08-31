7be8c9a37f805d6efe03cbdccbd9efaa31666d69
Pavlo Danylchenko <danilchenko@ukr.net>
2026-08-31 00:33:21 -0400

    Complete GSP 1143

--- dataplex_qwik_start_console/gsp1143.sh
+++ dataplex_qwik_start_console/gsp1143.sh
@@ -56,7 +56,7 @@ gcloud dataplex assets create measurements \
     --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID \
     --discovery-enabled
 
-read -p "Check the progress to verify the objective and Press [Enter] key to continue..."
+# read -p "Check the progress to verify the objective and Press [Enter] key to continue..."
 
 echo "======================================================================"
 echo "                   Task 4. Delete assets, zones, and lakes"
