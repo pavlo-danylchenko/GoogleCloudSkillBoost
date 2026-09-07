8a91cb999ad36a42f2dea231147f18b05191a367
Pavlo Danylchenko <danilchenko@ukr.net>
2026-09-07 00:00:04 -0400

    Complete GSP 540

--- engineer_ai_agents_with_agent_development_kit_adk_challenge_lab/gsp540.sh
+++ engineer_ai_agents_with_agent_development_kit_adk_challenge_lab/gsp540.sh
@@ -16,7 +16,12 @@ echo $REGION
 gcloud config set compute/zone $ZONE
 gcloud config set compute/region $REGION
 
-read -p "ENTER the MODEL NAME: " MODEL_NAME
+cd ~
+
+curl -O https://raw.githubusercontent.com/pavlo-danylchenko/GoogleCloudSkillBoost/refs/heads/main/engineer_ai_agents_with_agent_development_kit_adk_challenge_lab/geo_validator.py
+curl -O https://raw.githubusercontent.com/pavlo-danylchenko/GoogleCloudSkillBoost/refs/heads/main/engineer_ai_agents_with_agent_development_kit_adk_challenge_lab/llm_auditor.py
+curl -O https://raw.githubusercontent.com/pavlo-danylchenko/GoogleCloudSkillBoost/refs/heads/main/engineer_ai_agents_with_agent_development_kit_adk_challenge_lab/my_google_search_agent.py
+
 
 echo "======================================================================"
 echo "            Task 1. Install ADK and set up your environment"
@@ -24,7 +29,7 @@ echo "======================================================================"
 export PATH=$PATH:"/home/${USER}/.local/bin"
 python3 -m pip install google-adk
 
-gcloud auth application-default login
+gcloud auth application-default login --quiet
 
 gcloud storage cp gs://$DEVSHELL_PROJECT_ID-bucket/adk_project.zip .
 unzip adk_project.zip
@@ -35,16 +40,18 @@ pip install -r requirements.txt
 echo "======================================================================"
 echo "           Task 2. Initialize and Configure the Travel Scout"
 echo "======================================================================"
-FOLDERS=("my_google_search_agent" "fogeo_validatorlder2" "llm_auditor")
+FOLDERS=("my_google_search_agent" "geo_validator" "llm_auditor")
 
 for folder in "${FOLDERS[@]}"; do
+cp ~/$folder.py ~/adk_project/$folder/agent.py
 cat > ~/adk_project/$folder/.env << EOF
 GOOGLE_GENAI_USE_ENTERPRISE=true
 GOOGLE_CLOUD_PROJECT=$DEVSHELL_PROJECT_ID
 GOOGLE_CLOUD_LOCATION=global
-MODEL=$MODEL_NAME
+MODEL=gemini-3.5-flash
 EOF
 done
+
 
 echo "======================================================================"
 echo "               Task 3. Verify the agent via the CLI"
